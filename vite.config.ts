db222a0ab9c7f2a06af6725d72db0a115360e63c
ZomJeedz <onenarok777@gmail.com>
2025-11-26 10:42:39 +0700

    feat: add Google Tag Manager and AdSense integration with new environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      {
+        name: "html-transform",
+        transformIndexHtml(html) {
+          let newHtml = html.replace(
+            /%VITE_GOOGLE_TAG_MANAGER_ID%/g,
+            env.VITE_GOOGLE_TAG_MANAGER_ID || ""
+          );
+          newHtml = newHtml.replace(
+            /%VITE_GOOGLE_ADSENSE_ID%/g,
+            env.VITE_GOOGLE_ADSENSE_ID || ""
+          );
+          return newHtml;
+        },
+      },
+    ],
+  };
+});
